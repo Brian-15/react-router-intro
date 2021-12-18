1588a558a7974a4af1c4d3d9f72ecda989bcc049
Brian-15 <bcirieco@gmail.com>
2021-12-17 17:25:25 -0800

    add uuid to <li>

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Snack from './Snack.js';
 
 const VendingMachine = ({ snacks=['soda', 'chips', 'water'] }) => {
-
   return (
     <>
       <BrowserRouter>
         <Route exact path='/'>
           <ul>
           {snacks.map(snack => (
-            <li>
+            <li key={uuid()}>
               <Link to={`/${snack}`}>{snack}</Link>
             </li>
           ))}
