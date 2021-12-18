0e2f48b34f257a19198dec5a379e794da52a7947
Brian-15 <bcirieco@gmail.com>
2021-12-17 17:50:38 -0800

    changed react-router-dom to v5.2.1, altered components as necessary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { v4 as uuid } from 'uuid';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Snack from './Snack.js'
 import VendingMachine from './VendingMachine';
 
-function App() {
-  return <VendingMachine />;
+function App({ snacks=['soda', 'chips', 'water'] }) {
+  return (
+    <BrowserRouter>
+      <Route key={uuid()} exact path='/'>
+        <VendingMachine snacks={snacks} />
+      </Route>
+      {snacks.map(snack =>
+      <Route key={uuid()} exact path={`/${snack}`}>
+        <Snack key={uuid()} name={snack} />
+      </Route>)}
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import { v4 as uuid } from 'uuid';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Snack from './Snack.js';
+import { Link } from 'react-router-dom';
 
-const VendingMachine = ({ snacks=['soda', 'chips', 'water'] }) => {
+const VendingMachine = ({ snacks }) => {
+  console.log(snacks);
   return (
-    <>
-      <BrowserRouter>
-        <Route exact path='/'>
-          <ul>
-          {snacks.map(snack => (
-            <li key={uuid()}>
-              <Link to={`/${snack}`}>{snack}</Link>
-            </li>
-          ))}
-          </ul>
-        </Route>
-        {snacks.map(snack =>
-        <Route exact path={`/${snack}`}>
-          <Snack name={snack} />
-        </Route>)}
-      </BrowserRouter>
-    </>
+    <ul key={uuid()}>
+    {snacks.map(snack => (
+      <li key={uuid()}>
+        <Link key={uuid()} to={`/${snack}`}>{snack}</Link>
+      </li>
+    ))}
+    </ul>
   );
 };
 
